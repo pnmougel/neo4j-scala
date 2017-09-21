7728b4f779febfbb174e46e10a675fcaae2530ab
pnmougel <pnmougel@gmail.com>
2017-09-21 18:39:20 +0200

    add noAuth and fix connection url

--- src/main/scala/org/neo4jscala/Neo4jClient.scala
+++ src/main/scala/org/neo4jscala/Neo4jClient.scala
@@ -1,7 +1,8 @@
 package org.neo4jscala
 
-import org.neo4j.driver.v1.{AuthTokens, Driver, GraphDatabase, StatementResult}
+import org.neo4j.driver.v1._
 import org.neo4jscala.core.{Neo4jNode, NodeMapper}
+import scala.collection.JavaConversions._
 /**
   * Created by nico on 17/09/17.
   */
@@ -20,7 +21,6 @@ class Neo4jClient(driver: Driver) {
   def search[T <: Neo4jNode](implicit manifest: scala.reflect.Manifest[T]): Iterator[T] = {
     val className = manifest.runtimeClass.getSimpleName
     val result = run(s"MATCH (a:${className}) RETURN a")
-    import scala.collection.JavaConverters._
     asScalaIterator(result).map(record => {
       val node = record.get("a").asNode()
       NodeMapper.as[T](node)
@@ -37,11 +37,16 @@ class Neo4jClient(driver: Driver) {
 
 object Neo4jClientFactory {
   def usingBolt(url: String, user: String, password: String): Neo4jClient = {
-    val driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic(user, password))
+    val driver = GraphDatabase.driver(url, AuthTokens.basic(user, password))
     new Neo4jClient(driver)
   }
 
   def usingBolt(server: String = "localhost", port: Int = 7687, user: String = "neo4j", password: String): Neo4jClient = {
     usingBolt(s"bolt://$server:$port", user, password)
+  }
+
+  def noAuth(server: String = "localhost", port: Int = 7687) = {
+    val driver = GraphDatabase.driver(s"bolt://${server}:${port}")
+    new Neo4jClient(driver)
   }
 }
